942e4142568e054e8894f8289a49b788592edf47
Giang Thai Cuong <thaicuong.giang@gmail.com>
2018-02-12 10:08:44 +0700

    remove useless tests

--- tests/AbstractTestCase.php
+++ tests/AbstractTestCase.php
@@ -4,9 +4,6 @@ namespace Tests;
 
 use Mockery;
 use PHPUnit_Framework_TestCase;
-use Elasticsearch\ClientBuilder;
-use Elasticsearch\Client as Elastic;
-use Elasticsearch\Common\Exceptions\Curl\CouldNotConnectToHost;
 
 abstract class AbstractTestCase extends PHPUnit_Framework_TestCase
 {
@@ -15,33 +12,5 @@ abstract class AbstractTestCase extends PHPUnit_Framework_TestCase
         Mockery::close();
 
         parent::tearDown();
-    }
-
-    protected function getRealElasticsearchClient()
-    {
-        return ClientBuilder::create()
-            ->setHosts(['127.0.0.1:9200'])
-            ->setRetries(0)
-            ->build();
-    }
-
-    protected function resetIndex(Elastic $client)
-    {
-        $data = ['index' => 'table'];
-
-        if ($client->indices()->exists($data)) {
-            $client->indices()->delete($data);
-        }
-
-        $client->indices()->create($data);
-    }
-
-    protected function markSkippedIfMissingElasticsearch(Elastic $client)
-    {
-        try {
-            $client->cluster()->health();
-        } catch (CouldNotConnectToHost $e) {
-            $this->markTestSkipped('Could not connect to Elasticsearch');
-        }
     }
 }
